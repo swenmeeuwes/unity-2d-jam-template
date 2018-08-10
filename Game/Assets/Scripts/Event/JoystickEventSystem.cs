9e92e9b83b5fee5026a12137595f7fc938fc97e4
Swen <swenmeeu@gmail.com>
2018-08-10 23:59:10 +0200

    Implemented 'JoystickEventSystem'

---
+++ Game/Assets/Scripts/Event/JoystickEventSystem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Zenject;
+
+public class JoystickEventSystem : EventSystem
+{
+    #region Injected
+
+    private float _deadZone;
+
+    #endregion
+
+    [Inject]
+    private void Construct(float deadZone)
+    {
+        _deadZone = deadZone;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        if (firstSelectedGameObject == null)
+        {
+            firstSelectedGameObject = FindObjectOfType<Button>().gameObject;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (currentSelectedGameObject == null && (
+            Input.GetAxis(InputAxes.Horizontal) > _deadZone ||
+            Input.GetAxis(InputAxes.Horizontal) < -_deadZone ||
+            Input.GetAxis(InputAxes.Vertical) > _deadZone ||
+            Input.GetAxis(InputAxes.Vertical) < -_deadZone))
+        {
+            SetSelectedGameObject(firstSelectedGameObject);
+        }
+    }
+}
